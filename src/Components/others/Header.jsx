8c6f64977bbaef24c6cb72317992853b730e7ef0
candidcodes <candidpradhan@gmail.com>
2025-01-22 10:49:00 +0545

    fixed the login issue

--- src/Components/others/Header.jsx
+++ src/Components/others/Header.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import { UserContext } from '../../Context/AuthContext'
 
 const Header = ({ logged }) => {
-  const { saveUser } = useContext(UserContext)
+  const { saveUser, user } = useContext(UserContext)
 
   const logout = () => {
     localStorage.setItem('isLoggedIn', false)
@@ -10,11 +10,13 @@ const Header = ({ logged }) => {
     localStorage.removeItem('user')
     logged(false)
   }
-
   return (<>
     <div className='flex justify-between items-end'>
         <h1 className='text-2xl font-bold text-red-100'>Hi, <br/><span className='text-4xl font-bold text-red-100'>{saveUser.name}</span></h1>
-        <button onClick={logout} className='p-3 bg-red-600 text-gray-100 rounded-sm'>Log out</button>
+        <div className='flex gap-2'>
+          {user == 'admin' && <button onClick={() => {}} className='p-3 text-red-600 bg-gray-100 rounded-sm'>Add Employee +</button>}
+          <button onClick={logout} className='p-3 bg-red-600 text-gray-100 rounded-sm'>Log out</button>
+        </div>
     </div>
   </>
   )
